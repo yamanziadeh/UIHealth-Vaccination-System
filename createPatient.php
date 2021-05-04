d484ea673b371286e89640421f124937d7d8fd0e
Yaman Ziadeh <yamanziadeh@Yamans-MBP.hsd1.il.comcast.net>
2021-05-04 00:46:54 -0500

    Add createPatient.php

---
+++ createPatient.php
@@ -0,0 +1,41 @@
+<?php
+$dbhost = "localhost";$dbuser = "root";$dbpwd = "root";$dbname = "Project";
+
+$conn = new mysqli($dbhost,$dbuser,$dbpwd,$dbname);
+if($conn->connect_error) 
+{
+    echo "Error: could not connect to the DB";
+    exit;
+}
+
+$FName = $_POST["FName"];
+$MI = $_POST["MI"];
+$LName = $_POST["LName"];
+$SSN = $_POST["SSN"];
+$Address = $_POST["Address"];
+$Age = $_POST["Age"];
+$Gender = $_POST["Gender"];
+$Race = $_POST["Race"];
+$Occupation = $_POST["Occupation"];
+$medicalHistory = $_POST["medicalHistory"];
+$phoneNum = $_POST["phoneNum"];
+$Username = $_POST["Username"];
+$Password = $_POST["Password"];
+
+$myQ = "insert into patient (`SSN`,`FName`, `LName`, `MI`, `medicalHistory`, `address`, `age`, `gender`, `race`, `occupation`, `phoneNum`, `username`, `password`) 
+         values ('$SSN', '$FName', '$LName', '$MI', '$medicalHistory', '$Address', '$Age', '$Gender', '$Race', '$Occupation', '$phoneNum', '$Username', '$Password')";
+
+$results = $conn->query($myQ);
+
+$conn->close();
+
+if (isset($_SERVER["HTTP_REFERER"])) {
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+}
+
+/* 
+INSERT INTO patient (`SSN`,`FName`, `LName`, `MI`, `medicalHistory`, `address`, `age`, `gender`, `race`, `occupation`, `phoneNum`, `username`, `password`) 
+VALUES (333445555, "Yaman", "Ziadeh", "A", "Healthy", "213 southnorth St.", 20, "Other", "White", "Builder", 7082151497, "tMe", "patient")
+
+*/
+?>
--- home.php
+++ home.php
@@ -66,6 +66,7 @@
             <img class="mx-auto" src="logo.png" alt="">
         </div>
         <div id="homeDiv">
+        <div>
             <div class = "card">
                 <div class="card-body">
                     <center><h2 class="card-title">Login</h2></center>
@@ -91,23 +92,70 @@
                     </form>
                 </div>
             </div>
+            </div>
+            <div class="overflow-auto" style="height: 700px;">
             <div class = "card">
                 <div class="card-body">
                     <center><h2 class="card-title">Patient Registration</h2></center>
-                    <form action="home.php" method="POST">
-                        <div class="mb-3">
-                            <label for="username" class="form-label">Username</label>
-                            <input type="username" name="username" class="form-control" id="username">
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" class="form-control" id="password">
-                        </div>
+                    <form action="createPatient.php" method="POST">
+                    <div class="mb-3">
+                                <label for="FName" class="form-label">First Name:</label>
+                                <input type="FName" name="FName" class="form-control" id="FName">
+                            </div>
+                            <div class="mb-3">
+                                <label for="MI" class="form-label">Middle Initial:</label>
+                                <input type="MI" name="MI" class="form-control" id="MI">
+                            </div>
+                            <div class="mb-3">
+                                <label for="LName" class="form-label">Last Name:</label>
+                                <input type="LName" name="LName" class="form-control" id="LName">
+                            </div>
+                            <div class="mb-3">
+                                <label for="SSN" class="form-label">SSN:</label>
+                                <input type="SSN" name="SSN" class="form-control" id="SSN">
+                            </div>
+                            <div class="mb-3">
+                                <label for="Address" class="form-label">Address:</label>
+                                <input type="Address" name="Address" class="form-control" id="Address">
+                            </div>
+                            <div class="mb-3">
+                                <label for="Age" class="form-label">Age:</label>
+                                <input type="Age" name="Age" class="form-control" id="Age">
+                            </div>
+                            <div class="mb-3">
+                                <label for="Gender" class="form-label">Gender:</label>
+                                <input type="Gender" name="Gender" class="form-control" id="Gender">
+                            </div>
+                            <div class="mb-3">
+                                <label for="Race" class="form-label">Race:</label>
+                                <input type="Race" name="Race" class="form-control" id="Race">
+                            </div>
+                            <div class="mb-3">
+                                <label for="Occupation" class="form-label">Occupation:</label>
+                                <input type="Occupation" name="Occupation" class="form-control" id="Occupation">
+                            </div>
+                            <div class="mb-3">
+                                <label for="medicalHistory" class="form-label">Medical History:</label>
+                                <input type="medicalHistory" name="medicalHistory" class="form-control" id="medicalHistory">
+                            </div>
+                            <div class="mb-3">
+                                <label for="phoneNum" class="form-label">Phone Number:</label>
+                                <input type="phoneNum" name="phoneNum" class="form-control" id="phoneNum">
+                            </div>
+                            <div class="mb-3">
+                                <label for="Username" class="form-label">Username:</label>
+                                <input type="Username" name="Username" class="form-control" id="Username">
+                            </div>
+                            <div class="mb-3">
+                                <label for="Password" class="form-label">Password:</label>
+                                <input type="password" name="Password" class="form-control" id="Password">
+                            </div>
                         
                         <input type="submit" value="Register" class="btn btn-primary">
                     </form>
                 </div>
             </div>
+            <div>
         </div>
     </body>
 </html>
